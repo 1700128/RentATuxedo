d7e198aad1f546ef4fc3a81ab648e50665792f73
Henna <henna.oksanen@student.laurea.fi>
2018-12-10 18:39:31 +0200

    change

--- registration.php
+++ registration.php
@@ -4,28 +4,6 @@
 <!--Connection to database smokki, list of variables and sql query to send them into database smokki-->
   <?php
    include ('connection.php');
-   
-   $fname = $_POST['fname'];
-   $lname = $_POST['lname'] ;
-   $address = $_POST['address'] ;
-   $city = $_POST['city'] ;
-   $zipcode = $_POST['zipcode'];
-   $email = $_POST['email'];
-   $password = $_POST['password'];
-   
-   if (!$_POST ['submit']) {
-       echo "All fields are required";
-       }
-   else{
-       $sql = "INSERT into smokki (First Name, Last Name, Address, City, Postal Code, Email, Password)
-       values('$fname','$lname','$address','$city','$zipcode','$email','$password' ";
-       
-       if (mysqli_query($conn, $sql)){
-           echo "Data creation	succesfull, now you can Login";
-   }   
-       else{
-       echo "Something went wrong! Please try again.";}
-   
   ?>
 <head>  
   <meta charset="utf-8" />
