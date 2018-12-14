c136e67a067c6c0e8b813b7d9e68f8d3951f0ebd
Henna <henna.oksanen@student.laurea.fi>
2018-12-14 12:28:05 +0200

    commit

--- registration.php
+++ registration.php
@@ -3,9 +3,10 @@
   <head>
   <!--Connection to database smokki-->
   <?php
-  include ('reg.php');
-  include ('dbcontroller');
-?>
+   
+   include ('reg.php');
+
+  ?>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,7 +16,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     
-    <title>Registration</title>
+    <title>Contact</title>
     <style>
         .navbar {
         margin-bottom: 0;
@@ -33,54 +34,6 @@
     .bg-black {
         background-color: #000;
     }
-    .form-table {
-	background: #000;
-	width: 100%;
-	border-spacing: initial;
-	margin: 2px 0px;
-	word-break: break-word;
-	table-layout: auto;
-	line-height: 1.8em;
-	color: #FFF;
-	border-radius: 4px;
-	padding: 20px 20px;
-}
-
-.inputBox {
-	padding: 10px 30px;
-	border: #a9a9a9 1px solid;
-	border-radius: 4px;
-}
-.form-table td {
-	padding: 15px 0px;
-}
-.btnRegister {
-	padding: 30px 30px;
-	background-color: #000;
-	border: 0;
-	color: #FFF;
-	cursor: pointer;
-	border-radius: 4px;
-	margin-left: 20px;
-}
-body{
-	width:610px;
-	font-family:calibri;
-}
-.error-message {
-	padding: 7px 10px;
-	background: #fff1f2;
-	border: #ffd5da 1px solid;
-	color: #d6001c;
-	border-radius: 4px;
-}
-.success-message {
-	padding: 7px 10px;
-	background: #cae0c4;
-	border: #c3d0b5 1px solid;
-	color: #027506;
-	border-radius: 4px;
-}
     
   
    
@@ -103,46 +56,15 @@ body{
 </nav>
  	<div class="container text-center">
  	
- 	<h2>User Registration </h2> <br>
- 	<form name="frmRegistration" method="post" action="registration.php">
-<table border="0" width="500" align="center" class="demo-table">
-<?php if(!empty($success_message)) { ?>	
-<div class="success-message"><?php if(isset($success_message)) echo $success_message; ?></div>
-<?php } ?>
-<?php if(!empty($error_message)) { ?>	
-<div class="error-message"><?php if(isset($error_message)) echo $error_message; ?></div>
-<?php } ?>
-<tr>
-<td>User Name</td>
-<td><input type="text" class="demoInputBox" name="userName" value="<?php if(isset($_POST['userName'])) echo $_POST['userName']; ?>"></td>
-</tr>
-<tr>
-<td>First Name</td>
-<td><input type="text" class="demoInputBox" name="firstName" value="<?php if(isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></td>
-</tr>
-<tr>
-<td>Last Name</td>
-<td><input type="text" class="demoInputBox" name="lastName" value="<?php if(isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><input type="password" class="demoInputBox" name="password" value=""></td>
-</tr>
-<tr>
-<td>Confirm Password</td>
-<td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
-</tr>
-<tr>
-<td>Email</td>
-<td><input type="text" class="demoInputBox" name="userEmail" value="<?php if(isset($_POST['userEmail'])) echo $_POST['userEmail']; ?>"></td>
-</tr>
-<tr>
-<td colspan=2>
-<input type="checkbox" name="terms"> I accept Terms and Conditions <input type="submit" name="register-user" value="Register" class="btnRegister"></td>
-</tr>
-</table>
-</form>
-    
+ 	<h1>Please contact us, if you have any concerns. </h1>
+ 	<form method="post" action="registration.php">
+		Name : <input type="text" name="user_name" placeholder="Enter Your Name" /><br />
+        Address : <input type="text" name="user_address" placeholder="Enter Your Address" /><br />
+        City : <input type="text" name="user_citye" placeholder="Enter Your City" /><br />
+		Email : <input type="email" name="user_email" placeholder="Enter Your Email" /><br />
+		Password : <input name="user_password" /><br />
+		<input type="submit" value="Register" />
+		</form>
  	</div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
