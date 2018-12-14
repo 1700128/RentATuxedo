71e6e932c11f5abf8403cd1398ffcea813f5abb4
Henna <henna.oksanen@student.laurea.fi>
2018-12-14 09:17:30 +0200

    commit

--- registration.php
+++ registration.php
@@ -88,9 +88,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is comming from a form
     .bg-black {
         background-color: #000;
     }
-    .form horizontal input{
-  float:left;
-}
 
     
   
