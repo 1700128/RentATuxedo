67584ba905519092956443f4120be70518b2ea86
Henna <henna.oksanen@student.laurea.fi>
2018-12-16 19:17:57 +0200

    k

--- rental.php
+++ rental.php
@@ -42,6 +42,7 @@
     }
     .img-responsive{
         float:left;
+        size:70%;
     }
    
    
@@ -76,8 +77,6 @@
   <div class="col-sm-6">
   <img src="Pictures/suit.png" class="img-responsive"> 
   </div>
-  </div>
-  <div class="row">
   <div class="col-sm-6">
   <form class="form" method="post" action="rental.php">
 		Name : <input type="text" name="user_name" placeholder="Enter Your Name" /><br />
